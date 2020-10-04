228e8c448de96933740aa839309c463d79f4b651
faniabdo99 <faniabdo99@gmail.com>
2020-10-04 21:14:37 +0200

    Fix Https

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -44,7 +44,6 @@ class Kernel extends HttpKernel{
         'api' => [
             'throttle:60,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \App\Http\Middleware\HttpsMiddleware::class
         ],
     ];
 
